6e615d6abae71bac5a3d7857da9f0b4115ebc245
Ellen Schofield <ellen@pawprintsdesign.co.uk>
2021-04-06 12:02:29 +0100

    start of new NGS pipeline at Cambridge Uni

---
+++ ngs-pipeline.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+#! RUN : bash ngs-pipeline.sh <SAMPLE>
+#! Eg. : bash ngs-pipeline.sh CS_35365
+
+SAMPLE=$1
+SCRIPTS=`dirname $0`
+CFG="${SCRIPTS}/ngs-pipeline.config"
+
+[[ -z "$SAMPLE" ]] && { echo "ERROR: No SAMPLE provided for this run"; exit 1; }
+
+mkdir $SAMPLE; cd $SAMPLE
+cp $CFG $SAMPLE.config; source $SAMPLE.config
+mkdir -p ${GENOME}/logs
+
+jid1=$(sbatch -J ${SAMPLE}.fastq2sam ${SCRIPTS}/slurm/fastq2sam.sh ${SAMPLE})
+echo $jid1
---
+++ slurm/fastq2sam.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+#! RUN : sbatch fastq2sam.sh <TAG> <CHR>
+
+#! sbatch directives begin here ###############################
+#! Which project should be charged:
+#SBATCH -A MELLERSH-SL3-CPU
+#! How many whole nodes should be allocated?
+#SBATCH --nodes=1
+#! How many (MPI) tasks will there be in total? (<= nodes*32)
+#! The skylake/skylake-himem nodes have 32 CPUs (cores) each.
+#SBATCH --ntasks=12
+#! How much wallclock time will be required?
+#SBATCH --time 12:00:00
+#! What types of email messages do you wish to receive?
+#SBATCH --mail-type=ALL
+#! Uncomment this to prevent the job from being requeued (e.g. if
+#! interrupted by node failure or system downtime):
+##SBATCH --no-requeue
+#! For 6GB per CPU, set "-p skylake"; for 12GB per CPU, set "-p skylake-himem":
+#SBATCH -p skylake
+
+#SBATCH -o logs/job-%j.out
+
+module purge                               # Removes all modules still loaded
+module load rhel7/default-peta4            # REQUIRED - loads the basic environment
+
+module load bwa/0.7.12                     # bwa
+
+SAMPLE=$1
+source ${SAMPLE}.config
+
+bwa mem -M -t 12 ${FASTA}/${GENOME}.fasta ${WGS}/${SAMPLE}/${SAMPLE}\_R1.fastq.gz ${WGS}/${SAMPLE}/${SAMPLE}\_R2.fastq.gz > ${SAMPLE}\_aligned.sam
