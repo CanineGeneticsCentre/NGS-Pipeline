b3154b09101b77593b3bc2aa98574fd0048da76b
Ellen Schofield <ellen@pawprintsdesign.co.uk>
2021-08-13 10:45:23 +0100

    Update addRGinfo.sh

--- slurm/addRGinfo.sh
+++ slurm/addRGinfo.sh
@@ -9,14 +9,14 @@
 #! The skylake/skylake-himem nodes have 32 CPUs (cores) each.
 #SBATCH --ntasks=1
 #! How much wallclock time will be required?
-#SBATCH --time 12:00:00
+#SBATCH --time 01:00:00
 #! What types of email messages do you wish to receive?
 #SBATCH --mail-type=ALL
 #! Uncomment this to prevent the job from being requeued (e.g. if
 #! interrupted by node failure or system downtime):
 ##SBATCH --no-requeue
 #! For 6GB per CPU, set "-p skylake"; for 12GB per CPU, set "-p skylake-himem":
-#SBATCH -p skylake-himem
+#SBATCH -p skylake
 
 #SBATCH -o ../logs/job-%A_%a.out
 
