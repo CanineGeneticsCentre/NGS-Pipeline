e35576b8ad25451809f47066c425da039a902fab
Ellen Schofield <ellen@pawprintsdesign.co.uk>
2021-10-05 16:40:03 +0100

    Create baseRecalibrator.sh

---
+++ slurm/baseRecalibrator.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+#! RUN : sbatch sortSam.sh <SAMPLE>
+
+#! sbatch directives begin here ###############################
+#! How many whole nodes should be allocated?
+#SBATCH --nodes=1
+#! How many (MPI) tasks will there be in total? (<= nodes*32)
+#! The skylake/skylake-himem nodes have 32 CPUs (cores) each.
+#SBATCH --ntasks=1
+#! How much wallclock time will be required?
+#SBATCH --time 12:00:00
+#! What types of email messages do you wish to receive?
+#SBATCH --mail-type=ALL
+#! Uncomment this to prevent the job from being requeued (e.g. if
+#! interrupted by node failure or system downtime):
+##SBATCH --no-requeue
+#! For 6GB per CPU, set "-p skylake"; for 12GB per CPU, set "-p skylake-himem":
+#SBATCH -p skylake
+#SBATCH --mem=5gb
+
+#SBATCH -o ../logs/job-%A_%a.out
+
+module purge                                # Removes all modules still loaded
+module load rhel7/default-peta4             # REQUIRED - loads the basic environment
+
+module load jdk-8u141-b15-gcc-5.4.0-p4aaopt 
+module load gatk/4.1.0.0                    # GATK 4.1
+
+SAMPLE=$1
+source ${SAMPLE}.config
+
+
+intervals=`head -${SLURM_ARRAY_TASK_ID} sequence_grouping.txt | tail -${SLURM_ARRAY_TASK_ID} | sed s/"\t"/" -L "/g`
+
+gatk --java-options "-Djava.io.tmpdir=${HOME}/hpc-work/tmp/ -Xmx2G" BaseRecalibrator -R ${FASTA}/${GENOME}.fasta -I ${SAMPLE}.sorted.bam --use-original-qualities -O ${SAMPLE}.recal.grp --known-sites ${BQSR} -L ${intervals} --TMP_DIR ${HOME}/hpc-work/tmp/
