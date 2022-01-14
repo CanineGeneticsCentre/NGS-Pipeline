1870adc9a3f2309e2dce89575d4313338f4ebe7a
Ellen Schofield <ellen@pawprintsdesign.co.uk>
2022-01-14 10:02:38 +0000

    Update haplotypeCaller.sh

--- slurm/haplotypeCaller.sh
+++ slurm/haplotypeCaller.sh
@@ -33,4 +33,4 @@ source ${SAMPLE}.config
 
 intervals=`head -${SLURM_ARRAY_TASK_ID} sequence_grouping_with_unmapped.txt | tail -1 | sed s/"\t"/" -L "/g`
 
-gatk --java-options "-Djava.io.tmpdir=${HOME}/hpc-work/tmp/ -Xmx4G" HaplotypeCaller -R ${FASTA}/${GENOME}.fasta -I ${SAMPLE}-${REF}.bam -L ${intervals} -O ${SAMPLE}-${REF}.${SLURM_ARRAY_TASK_ID}.gvcf -GQB 10 -GQB 20 -GQB 30 -GQB 40 -GQB 50 -GQB 60 -GQB 70 -GQB 80 -GQB 90 -ERC GVCF
+gatk --java-options "-Djava.io.tmpdir=${HOME}/hpc-work/tmp/ -Xmx4G" HaplotypeCaller -R ${FASTA}/${GENOME}.fasta -I ${SAMPLE}-${REF}.bam -L ${intervals} -O ${SAMPLE}-${REF}.${SLURM_ARRAY_TASK_ID}.g.vcf -GQB 10 -GQB 20 -GQB 30 -GQB 40 -GQB 50 -GQB 60 -GQB 70 -GQB 80 -GQB 90 -ERC GVCF
