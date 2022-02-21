33afdf0c3d30b175450d4c62893ebc3665c2a11a
Ellen Schofield <ellen@pawprintsdesign.co.uk>
2022-02-21 15:23:16 +0000

    script for building the genomics db

---
+++ genomics-db.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+#! RUN : bash genomics-db.sh <SAMPLE> <REF>
+#! Eg. : bash genomics-db.sh sanples.list cf4
+
+SAMPLE_LIST=$1
+REF=$2
+SCRIPTS=`dirname $0`
+CFG="${SCRIPTS}/ngs-pipeline-${REF}.config"
+
+[[ -z "$SAMPLEs" ]] && { echo "ERROR: No list of SAMPLES provided for this run"; exit 1; }
+[[ -z "$REF" ]] && { echo "ERROR: No REFERENCE provided for this run"; exit 1; }
+
+date
+DIR=`echo $RANDOM | md5sum | head -c 10`
+
+echo "Creating directory ${DIR}"
+mkdir -p $DIR/logs; cd $DIR
+mv ../${SAMPLE_LIST} .
+
+cp $CFG ${REF}.config; source ${REF}.config
+
+
+for s in `cat ${SAMPLE_LIST}`; do
+  # Copy g.vcf files from RCS... will only copy if files no present or rcs version is newer
+  rsync --progress -av ${WGS}/${s}/${s}-${REF}.g.vcf* ./;
+done
+
+INTERVALS=`wc -l ${FASTA}/genomicsDB.intervals | awk '{print $1}'`
+
+jid1=$(sbatch -A ${ACCOUNT} -J GenomicsDB --array=1-${INTERVALS} ${SCRIPTS}/slurm/gvcf2GenomicsDB.sh ${SAMPLES} ${REF})
+
---
+++ slurm/gvcf2GenomicsDB.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+
+#! RUN : sbatch gvcf2GenomicsDB.sh <SAMPLE_LIST> <REF>
+
+#! sbatch directives begin here ###############################
+#! How many whole nodes should be allocated?
+#SBATCH --nodes=1
+#! How many (MPI) tasks will there be in total? (<= nodes*32)
+#! The skylake/skylake-himem nodes have 32 CPUs (cores) each.
+#SBATCH --ntasks=1
+#! How much wallclock time will be required?
+#SBATCH --time 06:00:00
+#! What types of email messages do you wish to receive?
+#SBATCH --mail-type=FAIL,INVALID_DEPEND
+#! Uncomment this to prevent the job from being requeued (e.g. if
+#! interrupted by node failure or system downtime):
+##SBATCH --no-requeue
+#! For 6GB per CPU, set "-p skylake"; for 12GB per CPU, set "-p skylake-himem":
+#SBATCH -p skylake
+#SBATCH --mem=5gb
+
+#SBATCH -o ../logs/job-%A_%a.out
+
+
+module purge                                # Removes all modules still loaded
+module load rhel7/default-peta4             # REQUIRED - loads the basic environment
+
+module load jdk-8u141-b15-gcc-5.4.0-p4aaopt 
+module load gatk/4.1.0.0
+
+SAMPLE_LIST=$1
+REF=$2
+source ${REF}.config
+
+
+INTERVALS=`head -${SLURM_ARRAY_TASK_ID} ${FASTA}/genomicsDB.intervals | tail -1 | sed s/" "/" -L "/g`
+CHR=`echo ${INTERVALS} | cut -f 1 -d' ' | cut -d'_' -f 1`
+
+if [[ ${#CHR} -lt 4 ]] ; then
+  CHR="chr"${CHR}
+fi
+
+
+GVCFs=""
+for s in `cat ${SAMPLE_LIST}`; do GVCFs+="-V ${s}-${REF}.g.vcf.gz "; done
+
+rm -rf ${GDB}/${GENOME}/${CHR}
+
+gatk GenomicsDBImport \
+    ${GVCFs} \
+    --tmp-dir=${HOME}/hpc-work/tmp/ \
+    --genomicsdb-workspace-path ${GDB}/${GENOME}/${CHR} \
+    -L ${INTERVALS} \
+    --overwrite-existing-genomicsdb-workspace
