9bd152ca386e9d00878e4d716a4951d946e9e93b
Ellen Schofield <ellen@pawprintsdesign.co.uk>
2022-03-29 12:04:25 +0100

    Create make-genomics-db.sh

---
+++ make-genomics-db.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+#! RUN : bash genomics-db.sh <SAMPLE> <REF> [<CHR>]
+#! Eg. : bash genomics-db.sh sanples.list cf4 [38]
+
+SAMPLE_LIST=$1
+REF=$2
+CHR=$3
+SCRIPTS=`dirname $0`
+CFG="${SCRIPTS}/ngs-pipeline-${REF}.config"
+
+[[ -z "$SAMPLE_LIST" ]] && { echo "ERROR: No list of SAMPLES provided for this run"; exit 1; }
+[[ -z "$REF" ]] && { echo "ERROR: No REFERENCE provided for this run"; exit 1; }
+
+#DIR='67c7afff2f'; cd $DIR; source ${REF}.config
+
+DIR=`echo $RANDOM | md5sum | head -c 10`
+echo "Creating directory ${DIR}"
+mkdir -p $DIR/logs; cd $DIR
+mv ../${SAMPLE_LIST} .
+cp $CFG ${REF}.config; source ${REF}.config
+
+
+for s in `cat ${SAMPLE_LIST}`; do
+  # Copy g.vcf files from RCS... will only copy if files no present or rcs version is newer
+  rsync --progress -av ${WGS}/${s}/${s}-${REF}.g.vcf* ./;
+done
+
+if [ -z "$CHR" ]; then
+  INTERVALS=`wc -l ${FASTA}/genomicsDB.intervals | awk '{print $1}'`
+  ARRAY="1-${INTERVALS}"
+else
+  ARRAY="${CHR}-${CHR}"
+fi
+
+echo sbatch -A ${ACCOUNT} -J GenomicsDB --array=${ARRAY} ${SCRIPTS}/slurm/createGenomicsDB.sh ${SAMPLE_LIST} ${REF}
+sbatch -A ${ACCOUNT} -J GenomicsDB --array=${ARRAY} ${SCRIPTS}/slurm/createGenomicsDB.sh ${SAMPLE_LIST} ${REF}
+
