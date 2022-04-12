7691bcbb2fcbca8348eafa49a08656e60e71859a
Ellen Schofield <ellen@pawprintsdesign.co.uk>
2022-04-12 13:21:22 +0100

    Update export-genomics-db.sh

--- export-genomics-db.sh
+++ export-genomics-db.sh
@@ -33,7 +33,7 @@ jid=$(sbatch -A ${ACCOUNT} -J ${REF}.VCF --array=${ARRAY} --export=SCRIPTS=${SCR
 if [ -z "$CHR" ]; then
   for chr in `cut -f 1 -d':' ${FASTA}/${REF}-genomicsDB.intervals | cut -f 1 -d' ' | cut -d'_' -f 1 | sort -n | uniq`; do
     sbatch -A ${ACCOUNT} -J ${REF}-$chr.snpEff --dependency=afterok:${jid1##* } --export=SCRIPTS=${SCRIPTS},REF=${REF} ${SCRIPTS}/slurm/annotateVcf.sh $chr;
-  }
+  done;
 else
   sbatch -A ${ACCOUNT} -J ${REF}-${CHR}.snpEff --dependency=afterok:${jid1##* } --export=SCRIPTS=${SCRIPTS},REF=${REF} ${SCRIPTS}/slurm/annotateVcf.sh $CHR;
 fi
