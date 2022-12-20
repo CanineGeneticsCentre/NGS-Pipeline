3a0be63e570b9126600da97c9aa12202d29fc8de
Ellen Schofield <es904@cam.ac.uk>
2022-12-20 14:34:48 +0000

    sync fastq.gz files

--- upload-enaFastq.sh
+++ upload-enaFastq.sh
@@ -9,7 +9,7 @@ SCRIPTS=`dirname $0`
 [[ -z "$SAMPLE" ]] && { echo "ERROR: No SAMPLE provided to upload"; exit 1; }
 
 mkdir -p $SAMPLE/logs; cd $SAMPLE
-rsync --progress -auvh ${WGS}/${SAMPLE}/*.fq.gz ./
+rsync --progress -auvh ${WGS}/${SAMPLE}/*.fastq.gz ./
 
 echo sbatch -J ${SAMPLE}.ascp --export=SAMPLE=${SAMPLE},CONDA_PREFIX,ASPERA_SCP_PASS ${SCRIPTS}/slurm/ena-ascp.sh
 sbatch -J ${SAMPLE}.ascp --export=SAMPLE=${SAMPLE},CONDA_PREFIX,ASPERA_SCP_PASS ${SCRIPTS}/slurm/ena-ascp.sh
