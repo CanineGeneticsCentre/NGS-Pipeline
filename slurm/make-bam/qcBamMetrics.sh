754786721cf1ca6a91255221da7b9bd830c54686
Ellen Schofield <10760087+tottlefields@users.noreply.github.com>
2023-05-10 18:25:21 +0100

    renamed/moved file

--- slurm/make-bam/qcBamMetrics.sh
+++ slurm/make-bam/qcBamMetrics.sh
@@ -19,35 +19,36 @@
 #SBATCH -p skylake
 #SBATCH --mem=5gb
 
-#SBATCH -o ../logs/bamMetrics-%j.out
+#SBATCH -o logs/bamMetrics-%j.out
 
 . /etc/profile.d/modules.sh                 # Leave this line (enables the module command)
 module purge                                # Removes all modules still loaded
 module load rhel7/default-peta4             # REQUIRED - loads the basic environment
 
-module load picard/2.9.2                    # Picard
-module load samtools/1.10                   # Samtools
 
 SAMPLE=$1
 REF=$2
 source ${SAMPLE}.config
 
+module load ${SAMTOOLS}
+module load $PICARD
+
 BAM_FILE=${SAMPLE}-${REF}.bam
 
 
-samtools flagstat ${BAM_FILE} > metrics/flagstat.out
-picard_latest CollectWgsMetrics INPUT=${BAM_FILE} O=metrics/collect_wgs_metrics.txt REFERENCE_SEQUENCE=${FASTA}/${GENOME}.fasta STOP_AFTER=100000000 VALIDATION_STRINGENCY=LENIENT
-picard_latest CollectInsertSizeMetrics INPUT=${BAM_FILE} O=metrics/insert_size_metrics.txt H=metrics/insert_size_histogram.pdf VALIDATION_STRINGENCY=LENIENT
+samtools flagstat ${BAM_FILE} > metrics/${SAMPLE}.flagstat
+picard_latest CollectWgsMetrics INPUT=${BAM_FILE} O=metrics/${SAMPLE}.wgs.txt REFERENCE_SEQUENCE=${FASTA}/${GENOME}.fasta STOP_AFTER=100000000 VALIDATION_STRINGENCY=LENIENT
+picard_latest CollectInsertSizeMetrics INPUT=${BAM_FILE} O=metrics/${SAMPLE}.insert_size.txt H=metrics/${SAMPLE}.insert_size_histogram.pdf VALIDATION_STRINGENCY=LENIENT
 
 echo ${SAMPLE}
 echo "..."
 
-grep 'in total' metrics/flagstat.out
-grep '%' metrics/flagstat.out | grep -v 'singleton'
+grep 'in total' metrics/${SAMPLE}.flagstat
+grep '%' metrics/${SAMPLE}.flagstat | grep -v 'singleton'
 echo "..."
 
-head -8 metrics/collect_wgs_metrics.txt | tail -2 | cut -f 2,3,15,16,18,20-22
+head -8 metrics/${SAMPLE}.insert_size.txt | tail -2 | cut -f 6,8
 echo "..."
 
-head -8 metrics/insert_size_metrics.txt | tail -2 | cut -f 6,8
+head -8 metrics/${SAMPLE}.wgs.txt | tail -2 | cut -f 2,3,15,16,18,20-22
 echo "..."
