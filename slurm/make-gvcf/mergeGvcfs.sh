c704e28bd0821d86c07ebb830e2ab06da64a377b
Ellen Schofield <10760087+tottlefields@users.noreply.github.com>
2023-05-11 15:23:44 +0100

    Create mergeGvcfs.sh

---
+++ slurm/make-gvcf/mergeGvcfs.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+#! RUN : sbatch haplotypeCaller.sh <SAMPLE>
+
+#! sbatch directives begin here ###############################
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --time 06:00:00
+#SBATCH --mail-type=FAIL,INVALID_DEPEND
+##SBATCH --no-requeue
+#SBATCH -p cclake
+
+#SBATCH -o logs/haplotypeCaller-%A_%a.out
+
+. /etc/profile.d/modules.sh                 # Leave this line (enables the module command)
+module purge
+module load rhel7/default-ccl
+
+
+SAMPLE=$1
+source ${SAMPLE}.config
+
+module load ${GATK}
+
+GVCFS=""
+for i in `seq 0 $(($INTERVALS-1))`; do n=$(printf "%04d" $i); GVCFS+="--variant gvcf/${SAMPLE}-${REF}.$n.g.vcf "; done
+
+
+gatk --java-options "-Djava.io.tmpdir=${HOME}/hpc-work/tmp/ -Xmx3G" MergeVcfs \
+  ${GVCFS} \
+  -O ${SAMPLE}-${REF}.g.vcf.gz
