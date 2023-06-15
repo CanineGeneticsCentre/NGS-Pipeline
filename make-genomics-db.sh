3c005d1d83c79a550d339fe605a0f5e29bd979db
Ellen Schofield <10760087+tottlefields@users.noreply.github.com>
2023-06-15 16:25:57 +0100

    v2 of building a genomicsDB

--- make-genomics-db.sh
+++ make-genomics-db.sh
@@ -3,40 +3,29 @@
 #! RUN : bash make-genomics-db.sh <SAMPLE> <REF> [<CHR>]
 #! Eg. : bash make-genomics-db.sh samples.list cf4 [38]
 
-SAMPLE_LIST=$1
+SAMPLE_MAP=$1
 REF=$2
-CHR=$3
 SCRIPTS=`dirname $0`
 CFG="${SCRIPTS}/ngs-pipeline-${REF}.config"
 
-[[ -z "$SAMPLE_LIST" ]] && { echo "ERROR: No list of SAMPLES provided for this run"; exit 1; }
+[[ -z "$SAMPLE_MAP" ]] && { echo "ERROR: No SAMPLE map provided for this run"; exit 1; }
 [[ -z "$REF" ]] && { echo "ERROR: No REFERENCE provided for this run"; exit 1; }
 
-#DIR='67c7afff2f'; cd $DIR; source ${REF}.config
-#DIR=`echo $RANDOM | md5sum | head -c 10`
-DIR=$(basename $SAMPLE_LIST .list)
+
+DIR=$(basename $SAMPLE_MAP .map)
 echo "Creating directory ${DIR}"
 mkdir -p $DIR/logs; cd $DIR
-cp ../${SAMPLE_LIST} .
+cp ../${SAMPLE_MAP} .
 cp $CFG ${REF}.config; source ${REF}.config
 
+module load ${GATK}
+#gatk SplitIntervals -R ${FASTA}/${GENOME}.fasta -L ${INTERVAL_LIST} --scatter-count ${INTERVALS} -O intervals --subdivision-mode BALANCING_WITHOUT_INTERVAL_SUBDIVISION
 
-for s in `cat ${SAMPLE_LIST}`; do
+
+for s in `cat ${SAMPLE_MAP}| cut -f 1`; do
   # Copy g.vcf files from RCS... will only copy if files no present or rcs version is newer
   rsync --progress -av ${WGS}/${s}/${s}-${REF}.g.vcf* ./;
 done
 
-
-# If chromosome is NOT set then use all lines in intervals list to scatter the update job
-if [ -z "$CHR" ]; then
-  INTERVALS=`wc -l ${FASTA}/${REF}-genomicsDB.intervals | awk '{print $1}'`
-  ARRAY="1-${INTERVALS}"
-else
-  MIN=`grep -n "^$CHR:" $FASTA/${REF}-genomicsDB.intervals | awk  -F':' ' { print $1 } ' | head -1`
-  MAX=`grep -n "^$CHR:" $FASTA/${REF}-genomicsDB.intervals | awk  -F':' ' { print $1 } ' | tail -1`
-  ARRAY="${MIN}-${MAX}"
-fi
-
-echo sbatch -A ${ACCOUNT} -J GenomicsDB --array=${ARRAY} ${SCRIPTS}/slurm/createGenomicsDB.sh ${SAMPLE_LIST} ${REF}
-sbatch -A ${ACCOUNT} -J GenomicsDB --array=${ARRAY} ${SCRIPTS}/slurm/createGenomicsDB.sh ${SAMPLE_LIST} ${REF}
+#sbatch -A ${ACCOUNT} -J GenomicsDB --array=0-$(($INTERVALS-1)) ${SCRIPTS}/slurm/createGenomicsDB.sh ${SAMPLE_MAP} ${REF}
 
